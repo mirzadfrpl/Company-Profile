27a3d6381382b6f3a092ba53a013d70651dc5ad4
mirzadfrpl <mirzadf123@gmail.com>
2024-10-07 08:44:02 +0700

    New File

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('nav');
     if (window.scrollY > 0) {
-        navbar.classList.add('shadow-md', 'border-b'); // Menambahkan bayangan dan border saat di-scroll
+        navbar.classList.add('shadow-md', 'border-b');
     } else {
-        navbar.classList.remove('shadow-md', 'border-b'); // Menghapus bayangan dan border saat di atas
+        navbar.classList.remove('shadow-md', 'border-b');
     }
 });
